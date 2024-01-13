9e2711d4d8c530397b96792e881ff605f5f3cc56
Jonatan Dacosta <dacostaafonso@gmail.com>
2024-01-13 01:40:02 +0100

    proyect + frontend

---
+++ src/presentation/template/chat-template.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from '../components';
+
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+
+export const ChatTemplate = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async ( text: string ) => {
+    
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text: text, isGpt: false }] );
+
+    //TODO: useCase
+    setIsLoading(false);
+
+    //TODO: Añadir el mensaje de isGpt en true
+  }
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 grid-y-2">
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudaré con las correcciones" />
+          {
+            messages.map((message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessage key={index} text="Esto es OpenAI" />
+                )
+                : (
+                  <MyMessage key={index} text={message.text} />
+                )
+            ))
+          }
+
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
+
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder='Escribe aquí'
+        disabledCorrections
+      />
+    </div>
+  )
+}
